ac49073616282acccfc30b5ee3348663605033d7
bhavikdabhi <bhavikdabhi1910@gmail.com>
2025-11-10 16:54:40 +0530

    new Activity add

---
+++ app/src/main/java/com/example/ecommerceapk/Activity/BagActivity.kt
@@ -0,0 +1,17 @@
+package com.example.ecommerceapk.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ecommerceapk.R
+
+class BagActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bag)
+
+    }
+}
